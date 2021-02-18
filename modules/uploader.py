755671ee135ffa3beafe22d3d4d06d15276d411b
Rate980 <xv0nnj0@icloud.com>
2021-02-18 21:13:35 +0900

    hotfix: template name

--- modules/uploader.py
+++ modules/uploader.py
@@ -8,4 +8,4 @@ app = flask.Blueprint('uploader', __name__)
 @app.route('/')
 def uproder_index():
 
-    return flask.render_template('uploader/index.html')
+    return flask.render_template('uploader/index.html.j2')
