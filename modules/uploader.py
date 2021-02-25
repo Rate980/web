fa9294e859245edccc62f74c101ab3918bf1f6ad
Rate980 <xv0nnj0@icloud.com>
2021-02-25 19:36:21 +0900

    test file uplord

--- modules/uploader.py
+++ modules/uploader.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import flask
+import database as db
 
 app = flask.Blueprint('uploader', __name__)
 
@@ -9,3 +10,14 @@ app = flask.Blueprint('uploader', __name__)
 def uproder_index():
 
     return flask.render_template('uploader/index.html.j2')
+
+
+@app.route('/upload', methods=['POST'])
+def uprod():
+    print(flask.request.files['uploadFile'])
+    # if flask.request.files != []:
+    #    id = db.set_file(flask.request.files[0])
+    #    return flask.render_template(id=id, server_name=flask.request.url_root
+    # )
+    file = flask.request.files["uploadFile"]
+    return f'{file.mimetype}, {file.filename}'
